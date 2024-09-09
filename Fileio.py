8328a37b996624edff31e79b0a02eba27a20bdb9
tatanakondakova <tnea@mail.ru>
2024-09-09 21:05:46 +0800

    Добавлена функция загрузки данных в формате json и отображения данных в двух списках

--- Fileio.py
+++ Fileio.py
@@ -7,6 +7,8 @@ import pyperclip
 import json
 import os
 
+from PyInstaller.utils.hooks.conda import files
+
 history_file = 'upload_history.json'
 
 
@@ -36,6 +38,26 @@ def upload():
     except Exception as e:
         mb.showerror('Ошибка', 'Произошла ошибка: {e}')
 
+
+def show_history():
+    if not os.path.exists(history_file):
+        mb.showinfo('История','История загрузок пуста')
+        return
+    history_window = Toplevel(window)
+    history_window.title('История загрузок')
+
+    files_listbox= Listbox(history_window, width=50, height=20)
+    files_listbox.grid(row=0, column=0, padx=(10, 0), pady=10)
+
+    links_listbox = Listbox(history_window, width=50, height=20)
+    links_listbox.grid(row=0, column=1, padx=(0, 10), pady=10)
+
+    with open(history_file, 'r') as f:
+        history = json.load(f)
+        for item in history:
+            files_listbox.insert(END, item[file_path])
+            links_listbox.insert(END, item[download_link])
+
 window = Tk()
 window.title('Сохранение файлов в облаке')
 window.geometry('400x200')
@@ -45,5 +67,8 @@ button.pack()
 
 entry = ttk.Entry()
 entry.pack()
+
+history_button= ttk.Button(text='Показать историю', command=show_history)
+history_button.pack()
 
 window.mainloop()
